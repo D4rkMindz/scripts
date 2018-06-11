7f5f625ba9bcef86c395c87e11e7fb81886a3877
Björn Pfoster <bjoern.pfoster@gmail.com>
2018-06-11 16:43:13 +0200

    Removed mysql (temporary)

--- vagrant/apache2_setup.sh
+++ vagrant/apache2_setup.sh
@@ -9,14 +9,14 @@
 #               To see "Call" to run this script to setup vagrant like described on 
 #               https://lernjournal.d4rkmindz.ch/doku.php/installationen:vagrant (german) 
 # Autor:        Björn Pfoster
-# Version:      1.0.1
+# Version:      1.0.2
 # Datum:        2018.06.11
 
 # Update
 sudo apt-get update && sudo apt-get -y dist-upgrade
 
 # Install system tools
-sudo apt-get -y install htop ncdu lynx ntp unzip #ccze
+sudo apt-get -y install htop ncdu lynx ntp unzip ccze
 
 # Install Apache
 sudo apt-get -y install apache2
@@ -24,7 +24,7 @@ sudo a2enmod rewrite ssl headers
 sudo service apache2 restart
 
 # Install MySQL, set MySQL root password
-sudo apt-get -y install mysql-server mysql-client
+# sudo apt-get -y install mysql-server mysql-client
 
 # Install PHP 7.2
 sudo apt-get -y install php7.2 libapache2-mod-php7.2 php7.2-mysql php7.2-curl php7.2-gd php7.2-intl php7.2-mcrypt php7.2-sqlite3 php7.2-mbstring php-gettext
